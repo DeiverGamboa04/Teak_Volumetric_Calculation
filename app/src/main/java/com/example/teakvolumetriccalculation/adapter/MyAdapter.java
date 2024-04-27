5a0b9c88ea7c66a05c0fc862a4b515fa35abd47b
Deiver Gamboa <jose.gamboa2017@uteq.edu.ec>
2024-04-27 10:37:54 -0500

    tercer diseño del repositorio

--- app/src/main/java/com/example/teakvolumetriccalculation/adapter/MyAdapter.java
+++ app/src/main/java/com/example/teakvolumetriccalculation/adapter/MyAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.teakvolumetriccalculation.R;
 import com.example.teakvolumetriccalculation.modelo.User;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -35,9 +38,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
         User user = userArrayList.get(position);
-        holder.imageUrl.setText(String.format("imageUrl = %s", user.getImageUrl()));
+        /*holder.imageUrl.setText(String.format("imageUrl = %s", user.getImageUrl()));*/
         holder.volumen.setText(String.format("%s", user.getVolumen()));
         holder.volumenAprox.setText(String.format("%s", user.getVolumenAprox()));
+
+
+        // Comprobar si el URL de la imagen existe y luego cargar la imagen usando Glide
+        if (user.getImageUrl() != null && !user.getImageUrl().isEmpty()) {
+            Glide.with(context1)
+                    .load(user.getImageUrl())
+                    .into(holder.imageUrl); // Asegúrate de tener un ImageView en tu ViewHolder
+        }
     }
 
     @Override
@@ -46,11 +57,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView volumen, volumenAprox, imageUrl;
+        TextView volumen, volumenAprox;
+        ImageView imageUrl;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageUrl = itemView.findViewById(R.id.textViewImaUrl);
+            imageUrl = itemView.findViewById(R.id.imageurltecaobservado);
             volumen = itemView.findViewById(R.id.textViewVolum);
             volumenAprox = itemView.findViewById(R.id.textViewVolumAprox);
         }
